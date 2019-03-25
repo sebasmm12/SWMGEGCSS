b4e7cb113ee727ae1917f475fec6212a1aa70500
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-03-24 23:15:33 -0500

    commit

---
+++ SWMGEGCSS/Scripts/Scripts_TP/scriptsLayout.js
@@ -0,0 +1,30 @@
+﻿window.onload = function () {
+    $.fn.extend({
+        animateCss: function (animationName, callback) {
+            var animationEnd = (function (el) {
+                var animations = {
+                    animation: 'animationend',
+                    OAnimation: 'oAnimationEnd',
+                    MozAnimation: 'mozAnimationEnd',
+                    WebKitAnimation: 'webkitAnimationEnd'
+                };
+                for (var t in animations) {
+                    if (el.style[t] !== undefined) {
+                        return animations[t];
+
+                    }
+                }
+            })(document.createElement('div'));
+            this.addClass('animated ' + animationName).one(animationEnd, function () {
+                $(this).removeClass('animated ' + animationName);
+                if (typeof callback === 'function') callback();
+            });
+            return this;
+        }
+    });
+    $("#cerrar_alerta").click(function () {
+        $(".alerta_servicio").animateCss('bounceOutRight', function () {
+            $(".alerta_servicio").addClass("cerrar_alerta");
+        });
+    });
+};
