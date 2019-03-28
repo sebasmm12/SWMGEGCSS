c5c60d06d2932dd3e9d46efbaeedc53d82214b12
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-03-28 01:20:24 -0500

    commit

---
+++ SWMGEGCSS/Scripts/Scripts_TP/scriptsCliente.js
@@ -0,0 +1,28 @@
+﻿window.onload = function () {
+    $.fn.extend({
+        animateCss: function (animationName, callback) {
+            var animationEnd = (function (el) {
+                var animations = {
+                    animation: 'animationend',
+                    OAnimation: 'oAnimationEnd',
+                    MozAnimation: 'mozAnimationEnd',
+                    WebKitAnimation: 'webkitAnimationEnd'
+                };
+                for (var t in animations) {
+                    if (el.style[t] !== undefined) {
+                        return animations[t];
+
+                    }
+                }
+            })(document.createElement('div'));
+            this.addClass('animated ' + animationName).one(animationEnd, function () {
+                $(this).removeClass('animated ' + animationName);
+                if (typeof callback === 'function') callback();
+            });
+            return this;
+        }
+    });
+    $(".nav-item").mouseenter(function () {
+        $(".nav-item").animateCss('pulse');
+    });
+};
