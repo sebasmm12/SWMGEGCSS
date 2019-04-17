451158aef2fda6fbbd0d1b3e81a829132d9fb9bc
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-04-17 00:35:59 -0500

    commit

--- SWMGEGCSS/Controllers/ClientController.cs
+++ SWMGEGCSS/Controllers/ClientController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SWMGEGCSS.Models;
+using SWMGEGCSS_EN;
 namespace SWMGEGCSS.Controllers
 {
     public class ClientController : Controller
@@ -27,6 +28,13 @@ namespace SWMGEGCSS.Controllers
         }
         public ActionResult Contactenos()
         {
+             SolicitudViewModel solicitud=new SolicitudViewModel();
+            return View(solicitud);
+        }
+        [HttpPost]
+        public ActionResult Contactenos(T_Solicitud Solicitud)
+        {
+            string m = Solicitud.mensaje;
             return View();
         }
     }
