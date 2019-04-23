c50df75c7fca6ce8ea65dfa3fb7f2252a0410296
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-04-23 00:25:16 -0500

    commit

--- SWMGEGCSS/Controllers/GerenteController.cs
+++ SWMGEGCSS/Controllers/GerenteController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Common;
+using SWMGEGCSS_DA.Base;
+using SWMGEGCSS_EN;
 
 namespace SWMGEGCSS.Controllers
 {
     public class GerenteController : Controller
     {
+
         // GET: Gerente
         public ActionResult Index()
         {
