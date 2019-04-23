37302b9e4aa123e231d6a3c1e6d85f78a1c423d1
USER LENOVO <USER LENOVO@LENOVO>
2019-04-23 02:40:42 -0500

    commit

---
+++ SWMGEGCSS/Controllers/PlanController.cs
@@ -0,0 +1,29 @@
+﻿using SWMGEGCSS.Models;
+using SWMGEGCSS_EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SWMGEGCSS.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET: Plan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Agregar_Plan_de_Proyectos()
+        {
+            var model = new GestionarPlanProyectoViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Agregar_Plan_de_Proyectos(T_plan plan)
+        {
+            return View();
+        }
+    }
+}
