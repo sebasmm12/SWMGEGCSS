2a20aa96d9044fb3e2c2674b125c3a27c35b33c0
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-04-26 03:45:16 -0500

    commit

--- SWMGEGCSS/Scripts/Scripts_TP/MIRANDA/scriptsGestionarPlanProyecto.js
+++ SWMGEGCSS/Scripts/Scripts_TP/MIRANDA/scriptsGestionarPlanProyecto.js
@@ -7,26 +7,44 @@
         $input.autocomplete(options);
 
     };
-
     $(".btnModal").each(function () {
         $(this).click(function () {
             var $button = $(this);
+            var modal = $button.attr("data-id-target");
             $.ajax({
                 url: $(this).attr("data-url"),
                 method: "POST",
-                data: { id: $(this).attr("data-id-proyecto")}
+                data: { id: $(this).attr("data-id-proyecto") }
             }).done(function (data) {
-                var $target = $($button.attr("data-id-target"));
-                var modal = $button.attr("data-id-target");
+               var $target = $($button.attr("data-id-target"));
                 var $newhtml = $(data);
                 $target.replaceWith($newhtml);
-                $(modal).modal('show');
+                $(modal).modal();
+                $(modal).on('shown.bs.modal', function () {
+                    $(document).off('focusin.modal');
+                });
+                $(".btnEliminar").each(function () {
+                    $(this).click(function () {
+                       
+                        (async function getEmail() {
+                            const { value: email } = await Swal.fire({
+                                title: 'Input email address',
+                                input: 'email',
+                                inputPlaceholder: 'Enter your email address'
+                            })
 
-            });
-
-           
+                            if (email) {
+                                Swal.fire('Entered email: ' + email)
+                            }
+                        })()
+                        
+                    });
+               });
+                });
         });
+        
     });
+
     $("input[data-exp-autocomplete]").each(autcompletado);
 
     
