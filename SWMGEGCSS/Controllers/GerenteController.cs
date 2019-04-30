5e25bfcfda749b1b4cda305cf43757b5554c69d0
carloslau2709 <carloslau2709@hotmail.com>
2019-04-30 01:27:36 -0500

    commi

--- SWMGEGCSS/Controllers/GerenteController.cs
+++ SWMGEGCSS/Controllers/GerenteController.cs
@@ -85,7 +85,7 @@ namespace SWMGEGCSS.Controllers
         {
             GestionarEmpresaViewModel model = new GestionarEmpresaViewModel();
            
-            model.listEmpresas = new EmpresaDataAccess().sp_Consultar_Lista_Empresa().ToPagedList(page, 2);
+            model.listEmpresas = new EmpresaDataAccess().sp_Consultar_Lista_Empresa().ToPagedList(page, 4);
             return View(model);
         }
         public ActionResult Visualizar_Personal_Proyecto()
