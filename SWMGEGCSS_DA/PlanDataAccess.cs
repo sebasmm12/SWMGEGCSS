319619b602f3becdb91fa393d2a5858454fbbb10
USER LENOVO <USER LENOVO@LENOVO>
2019-05-01 21:22:38 -0500

    commit

--- SWMGEGCSS_DA/PlanDataAccess.cs
+++ SWMGEGCSS_DA/PlanDataAccess.cs
@@ -84,7 +84,7 @@ namespace SWMGEGCSS_DA
                             T_plan_aux t_plan_aux = new T_plan_aux();
                             t_plan_aux.plan_nombre = DataUtil.DbValueToDefault<string>(reader["plan_nombre"]);
                             t_plan_aux.plan_fecha = DataUtil.DbValueToDefault<DateTime>(reader["plan_fecha"]);
-                            t_plan_aux.plan_estado_nobre = DataUtil.DbValueToDefault<string>(reader["plan_estado"]);
+                            t_plan_aux.plan_estado_nobre = DataUtil.DbValueToDefault<string>(reader["plan_estado_nobre"]);
                             t_plan_aux.plan_costo = DataUtil.DbValueToDefault<double>(reader["plan_costo"]);
                             t_plan_aux.plan_tiempo = DataUtil.DbValueToDefault<int>(reader["plan_tiempo"]);
                             T_Plan.Add(t_plan_aux);
