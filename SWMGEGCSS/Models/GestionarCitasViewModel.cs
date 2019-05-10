89078942c09369ffd29a4f17149d6003502c7840
Mauricio <magincho1000@gmail.com>
2019-05-09 23:16:07 -0500

    commit

---
+++ SWMGEGCSS/Models/GestionarCitasViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SWMGEGCSS_EN;
+using PagedList;
+using SWMGEGCSS_EN;
+using System.Collections.Generic;
+
+namespace SWMGEGCSS.Models
+{
+    public class GestionarCitasViewModel
+    {
+
+        public T_Citas citas { get; set; }
+
+        public List<T_Citas> listcitas { get; set; }
+
+        public IPagedList<T_Citas> listCitas { get; set; }
+
+       
+
+
+    }
+}
