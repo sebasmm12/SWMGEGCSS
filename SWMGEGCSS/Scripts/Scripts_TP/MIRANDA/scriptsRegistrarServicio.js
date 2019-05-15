894c6c3d296f68e9c333fa1ad6416fe6cb3f0b5d
sebastian ernesto miranda manrique <sebasmm12@hotmail.com>
2019-05-15 03:45:15 -0500

    commit

--- SWMGEGCSS/Scripts/Scripts_TP/MIRANDA/scriptsRegistrarServicio.js
+++ SWMGEGCSS/Scripts/Scripts_TP/MIRANDA/scriptsRegistrarServicio.js
@@ -1,4 +1,5 @@
 ﻿$(function () {
+    var modal;
     var añadir = function () {
         var $button = $(this);
         var selector = document.getElementById("actividad");
@@ -14,13 +15,14 @@
             $(target).replaceWith($newhtml);
             $(".btnModal").each(envioajaxModal);
             $(".btnEliminar").each(eliminarActividad);
+            $(".btnRegistrar").each(registrarActividadParcial);
             });
         return true;
     };
     var envioajaxModal = function () {
         $(this).click(function () {
             var $button = $(this);
-            var modal = $button.attr("data-id-target");
+            modal = $button.attr("data-id-target");
             $.ajax({
                 url: $button.attr("data-url"),
                 data: {
@@ -33,6 +35,7 @@
                 $target.replaceWith($newhtml);
                 $(modal).modal();
                 $(".btnEliminar").each(eliminarActividad);
+                $(".btnRegistrar").each(registrarActividadParcial);
             });
         });
         return true;
@@ -53,13 +56,72 @@
                 $(target).replaceWith($newhtml);
                 $(".btnModal").each(envioajaxModal);
                 $(".btnEliminar").each(eliminarActividad);
+                $(".btnRegistrar").each(registrarActividadParcial);
             });
         });
         
         return true;
     };
+    var registrarActividadParcial = function () {
+        $(this).click(function () {
+            var $button = $(this);
+            let params = new URLSearchParams(location.search);
+            var page = params.get('page');
+            var actividad = {
+                act_id : $button.attr("data-id-act"),
+                act_nombre: document.getElementById("actividades_act_nombre").value,
+                act_descripcion: document.getElementById("actividades_act_descripcion").value,
+                costo: document.getElementById("actividades_costo").value,
+                act_obligatorio: document.getElementById("est_actividad").value
+            };
+            $.ajax({
+                url: "/Servicios/ModificarDatosActividad",
+                data: {
+                    actividad: actividad,
+                    page: page
+                },
+                type: "POST"
+
+            }).done(function (data) {
+                var $newhtml = $(data);
+                var target = document.getElementById("tableAct");
+                $(target).replaceWith($newhtml);
+                $(modal).modal('hide');
+                $("#btnRegistrarActividad").click(añadir);
+                $(".btnModal").each(envioajaxModal);
+                $(".btnEliminar").each(eliminarActividad);
+            });
+        });
+    };
+    var registrarServicioCompleto = function () {
+        var servicio = {
+            tipo_servicio_nombre: document.getElementById("tipoServicio_tipo_servicio_nombre").value,
+            tipo_servicio_descripcion: document.getElementById("tipoServicio_tipo_servicio_descripcion").value
+        };
+        $.ajax({
+            url: "/Servicios/RegistrarServicioActividad",
+            data: {
+                servicio: servicio
+            },
+            type: "POST"
+
+        }).done(function (data) {
+            Swal.fire({
+                type: 'success',
+                title: 'Se registro el servicio exitosamente',
+                confirmButtonText: 'OK'
+
+            }).then((result) => {
+                if (result.value) {
+                    window.location.href = "/Servicios/VisualizarServicios";
+                }
+            });   
+        });
+    };
 
     $("#btnRegistrarActividad").click(añadir);
     $(".btnModal").each(envioajaxModal);
     $(".btnEliminar").each(eliminarActividad);
+    $(".btnRegistrar").each(registrarActividadParcial);
+    $("#btnRegistrarServicio").click(registrarServicioCompleto);
 });
--- SWMGEGCSS_EN/T_tipo_servicio_actividades.cs
+++ SWMGEGCSS_EN/T_tipo_servicio_actividades.cs
@@ -13,4 +13,13 @@ namespace SWMGEGCSS_EN
         public double costo { get; set; }
         public bool act_obligatorio { get; set; }
     }
+    public class T_tipo_servicio_actividades_aux
+    {
+        public int tipo_servicio_id { get; set; }
+        public int act_id { get; set; }
+        public double tipo_servicio_costo { get; set; }
+        public bool tipo_servicio_obligatorio { get; set; }
+        public string tipo_servicio_nombre { get; set; }
+        public string act_nombre { get; set; }
+    }
 }
