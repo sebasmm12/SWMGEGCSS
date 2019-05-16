0e4c7c666813491f06b9ebd90731197ad8e90c5d
USER LENOVO <USER LENOVO@LENOVO>
2019-05-16 00:38:01 -0500

    commiy

---
+++ SWMGEGCSS/Models/TareasAsignadasViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SWMGEGCSS_EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SWMGEGCSS.Models
+{
+    public class TareasAsignadasViewModel
+    {
+        public T_actividades_desarrollar
+    }
+}
