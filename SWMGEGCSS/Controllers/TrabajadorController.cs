65203da5dc241ca2245ed18b8808ba64a4fed22d
USER LENOVO <USER LENOVO@LENOVO>
2019-05-17 02:12:40 -0500

    C

--- SWMGEGCSS/Controllers/TrabajadorController.cs
+++ SWMGEGCSS/Controllers/TrabajadorController.cs
@@ -18,12 +18,12 @@ namespace SWMGEGCSS.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase imagen,int p)
+        public ActionResult Index(HttpPostedFileBase imagen, int p)
         {
-            if(imagen!=null && imagen.ContentLength > 0)
+            if (imagen != null && imagen.ContentLength > 0)
             {
                 byte[] imagenData = null;
-                using (var binaryImagen= new BinaryReader(imagen.InputStream))
+                using (var binaryImagen = new BinaryReader(imagen.InputStream))
                 {
                     imagenData = binaryImagen.ReadBytes(imagen.ContentLength);
                 }
@@ -47,6 +47,48 @@ namespace SWMGEGCSS.Controllers
             int Usuario = (int)Session["login"];
             model.PLista_Actividades_a_Desarrollar = new TrabajadorDataAccess().sp_listar_plan_por_usuario_asignado(Usuario).ToPagedList(page, 4);
             return View(model);
+        }
+        [HttpGet]
+        public ActionResult AgregarArchivo(int id)
+        {
+            TareasAsignadasViewModel model = new TareasAsignadasViewModel();
+            Session["ArchivoId"] = id;
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult AgregarArchivo(HttpPostedFileBase archivo, T_actividades_desarrollar t_act_desa)
+        {
+            String ruc = "";
+            int planid = 0;
+            if (archivo != null)
+            {
+                String ruta = Server.MapPath("~/"+ ruc + "/" + planid.ToString() + "/");
+                ruta += archivo.FileName;
+                subirArchivo(archivo,ruta);
+            }
+            var model = new ActividadViewModel();
+            model.Actividades_Desarrollar = t_act_desa;
+
+            var modelAct = new T_actividades_desarrollar();
+            modelAct.act_desa_id = (int)Session["ArchivoId"];
+            modelAct.act_desa_archivo_nombre = t_act_desa.act_desa_archivo_nombre;
+            modelAct.act_desa_archivourl = t_act_desa.act_desa_archivourl;
+            modelAct.act_desa_comentario = t_act_desa.act_desa_comentario;
+
+            var operationResult = new TrabajadorDataAccess().sp_Agregar_Tarea_Asignada(modelAct);
+
+            return Json(new { data = operationResult.NewId }, JsonRequestBehavior.AllowGet);
+        }
+        public void subirArchivo(HttpPostedFileBase archivo,String ruta)
+        {
+            try
+            {
+                archivo.SaveAs(ruta);
+            }
+            catch (Exception ez)
+            {
+
+            }
         }
     }
 }
--- SWMGEGCSS_DA/TrabajadorDataAccess.cs
+++ SWMGEGCSS_DA/TrabajadorDataAccess.cs
@@ -70,6 +70,7 @@ namespace SWMGEGCSS_DA
                         while (reader.Read())
                         {
                             T_actividades_desarrollar t_Act_des = new T_actividades_desarrollar();
+                            t_Act_des.act_desa_id = DataUtil.DbValueToDefault<int>(reader["act_desa_id"]);
                             t_Act_des.act_desa_nombre = DataUtil.DbValueToDefault<string>(reader["act_desa_nombre"]);
                             t_Act_des.act_desa_fecha_inicio = DataUtil.DbValueToDefault<DateTime>(reader["act_desa_fecha_inicio"]);
                             t_Act_des.act_desa_fecha_fin = DataUtil.DbValueToDefault<DateTime>(reader["act_desa_fecha_fin"]);
@@ -84,6 +85,27 @@ namespace SWMGEGCSS_DA
                 return new List<T_actividades_desarrollar>();
             }
             return lista_actividades_desarrollar;
+        }
+        public OperationResult sp_Agregar_Tarea_Asignada(T_actividades_desarrollar t_act_desa)
+        {
+            try
+            {
+                var operationresult = new OperationResult();
+                using (DbCommand command = Database.GetStoredProcCommand("sp_Agregar_Tarea_Asignada"))
+                {
+                    Database.AddInParameter(command, "@act_desa_id", DbType.Int32, t_act_desa.act_desa_id);
+                    Database.AddInParameter(command, "@act_desa_archivo_nombre", DbType.String, t_act_desa.act_desa_archivo_nombre);
+                    Database.AddInParameter(command, "@act_desa_archivourl", DbType.String, t_act_desa.act_desa_archivourl);
+                    Database.AddInParameter(command, "@act_desa_comentario", DbType.String, t_act_desa.act_desa_comentario);
+                    Database.ExecuteScalar(command);
+                    operationresult.NewId = 1;
+                }
+                return operationresult;
+            }
+            catch (Exception ex)
+            {
+                return new OperationResult();
+            }
         }
     }
 }
