e605d833b24b539fc0498d93b1238cb2593ac9d5
USER LENOVO <USER LENOVO@LENOVO>
2019-06-12 12:44:51 -0500

    commit

---
+++ SWMGEGCSS/Scripts/Scripts_TP/PIERO/scriptsVisualizarAuditoriaActividades.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var getPage = function () {
+        var $a = $(this);
+        $.ajax({
+            url: $a.attr("href"),
+            type: "GET"
+        }).done(function (data) {
+            var target = $a.parents("div.pagedList").attr("data-exp-target");
+            $(target).replaceWith(data);
+        });
+        return false;
+    };
+    $(".pcoded-content").on("click", ".pagedList a", getPage);
+});
--- SWMGEGCSS_DA/AuditoriaExpedienteDataAccess.cs
+++ SWMGEGCSS_DA/AuditoriaExpedienteDataAccess.cs
@@ -43,5 +43,38 @@ namespace SWMGEGCSS_DA
             }
             return listAuditoriaAux;
         }
+        public List<T_auditoria_actividades_desarrollo> sp_Listar_Auditoria_Actividades_Desarrollar_por_ActividadesId(int act_desa_id)
+        {
+            List<T_auditoria_actividades_desarrollo> listAuditoriaAux = new List<T_auditoria_actividades_desarrollo>();
+            try
+            {
+                using (DbCommand command = Database.GetStoredProcCommand("sp_Listar_Auditoria_Actividades_Desarrollar_por_ActividadesId"))
+                {
+                    Database.AddInParameter(command, "@act_desa_id", DbType.Int32, act_desa_id);
+                    using (IDataReader reader = Database.ExecuteReader(command))
+                    {
+                        while (reader.Read())
+                        {
+                            T_auditoria_actividades_desarrollo t_aux_audit_act = new T_auditoria_actividades_desarrollo();
+                            t_aux_audit_act.act_desa_id = DataUtil.DbValueToDefault<int>(reader["act_desa_id"]);
+                            t_aux_audit_act.audi_act_fecha_auditoria = DataUtil.DbValueToDefault<DateTime>(reader["audi_act_fecha_auditoria"]);
+                            t_aux_audit_act.audi_act_fecha_inicio = DataUtil.DbValueToDefault<DateTime>(reader["audi_act_fecha_inicio"]);
+                            t_aux_audit_act.audi_act_fecha_fin = DataUtil.DbValueToDefault<DateTime>(reader["audi_act_fecha_fin"]);
+                            t_aux_audit_act.audi_act_fecha_revision = DataUtil.DbValueToDefault<DateTime>(reader["audi_act_fecha_revision"]);
+                            t_aux_audit_act.audi_act_fecha_finalizada = DataUtil.DbValueToDefault<DateTime>(reader["audi_act_fecha_finalizada"]);
+                            t_aux_audit_act.usu_asignado = DataUtil.DbValueToDefault<int>(reader["usu_asignado"]);
+                            t_aux_audit_act.audi_act_archivo_url = DataUtil.DbValueToDefault<String>(reader["audi_act_archivo_url"]);
+                            t_aux_audit_act.audi_act_archivo_nombre = DataUtil.DbValueToDefault<String>(reader["audi_act_archivo_nombre"]);
+                            listAuditoriaAux.Add(t_aux_audit_act);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return new List<T_auditoria_actividades_desarrollo>();
+            }
+            return listAuditoriaAux;
+        }
     }
 }
