99e89586f89b5126b2f70c3ca85deae016d0cbf9
carloslau2709 <carloslau2709@hotmail.com>
2019-06-16 06:38:50 -0500

    commit

--- SWMGEGCSS/Scripts/Scripts_TP/LAO LAM/scriptActualizar_Detalle_Usuario.js
+++ SWMGEGCSS/Scripts/Scripts_TP/LAO LAM/scriptActualizar_Detalle_Usuario.js
@@ -55,9 +55,58 @@
         return false;
     };
 
-       
+    var esNum = function esNumero(txt) {
+        if (isNaN(txt)) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    var esnegativo = function esNegativo(X) {
+        if (X < 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
     var maximoNumeroCaracteres50 = function maxCharacters(X) {
         if (X.length > 50) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    var maximoNumeroCaracteres70 = function maxCharacters(X) {
+        if (X.length > 70) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    var tieneCaracEsp = function empiezaConCaracteresEspeciales(X) {
+        var iChars = "!@_$%^&*()+=[]\\\';,/{}|\":<>?";
+      
+        for (var i = 0; i < X.length; i++) {
+            if (iChars.indexOf(X.charAt(i)) !== -1) {//numero es un CE o un numero
+                return true;
+            }
+           
+        }
+        return false;
+    };
+
+    var maximoNumeroCaracteres9 = function maxCharacters(X) {
+        if (X.length > 9) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    var minimoNumeroCaracteres7 = function maxCharacters(X) {
+        if (X.length < 7) {
             return true;
         } else {
             return false;
@@ -81,6 +130,13 @@
                 $("#det_usu_nombre").keyup(KeyNombre);
                 return false;
             }
+            else if (maximoNumeroCaracteres50(nombre)===true) {
+                adderror("det_usu_nombre");
+                negativeattributes("error-det-usu-nombre", 'Ingrese un nombre menor a 50 caracteres');
+                $("#det_usu_nombre").focus();
+                $("#det_usu_nombre").keyup(KeyNombre);
+                return false;
+            }
             else {
                 addgood("det_usu_nombre");
                 attributes("error-det-usu-nombre");
@@ -100,6 +156,10 @@
             adderror("det_usu_nombre");
             negativeattributes("error-det-usu-nombre", 'Ingrese nombre');
         }
+        else if (maximoNumeroCaracteres50($valor.val())==true) {
+            adderror("det_usu_nombre");
+            negativeattributes("error-det-usu-nombre", 'Ingrese un nombre menor a 50 caracteres');
+        }
         else {
             addgood("det_usu_nombre");
             attributes("error-det-usu-nombre");
@@ -111,7 +171,22 @@
         function validar_det_usu_direccion(direccion) {
             if (direccion === "") {
                 adderror("det_usu_direccion");
+                negativeattributes("error-det-usu-direccion", 'Ingrese direccion');
+                $("#det_usu_direccion").focus();
+                $("#det_usu_direccion").keyup(KeyDireccion);
+                return false;
+            }
+            else if (tieneCaracEsp(direccion) === true) {
+                adderror("det_usu_direccion");
                 negativeattributes("error-det-usu-direccion", 'Ingrese direccion válida');
+                $("#det_usu_direccion").focus();
+                $("#det_usu_direccion").keyup(KeyDireccion);
+                return false;
+
+            }
+            else if (maximoNumeroCaracteres70(direccion)===true) {
+                adderror("det_usu_direccion");
+                negativeattributes("error-det-usu-direccion", 'Ingrese una dirección menor a 70 caracteres');
                 $("#det_usu_direccion").focus();
                 $("#det_usu_direccion").keyup(KeyDireccion);
                 return false;
@@ -128,8 +203,15 @@
 
         if ($valor.val() === "") {
             adderror("det_usu_direccion");
+            negativeattributes("error-det-usu-direccion", 'Ingrese direccion');
+        }
+        else if (tieneCaracEsp($valor.val())===true) {
+            adderror("det_usu_direccion");
             negativeattributes("error-det-usu-direccion", 'Ingrese direccion válida');
-
+        }
+        else if (maximoNumeroCaracteres70($valor.val())===true) {
+            adderror("det_usu_direccion");
+            negativeattributes("error-det-usu-direccion", 'Ingrese una dirección menor a 70 caracteres');
         }
         else {
             addgood("det_usu_direccion");
@@ -144,6 +226,41 @@
             if (telefono.match(RegularExpression)) {
                 adderror("det_usu_telefono");
                 negativeattributes("error-det-usu-telefono", 'Ingrese telefono válida');
+                $("#det_usu_telefono").focus();
+                $("#det_usu_telefono").keyup(KeyTelefono);
+                return false;
+            }
+            else if (telefono === "") {
+                adderror("det_usu_telefono");
+                negativeattributes("error-det-usu-telefono", 'Ingrese telefono');
+                $("#det_usu_telefono").focus();
+                $("#det_usu_telefono").keyup(KeyTelefono);
+                return false;
+            }
+            else if (minimoNumeroCaracteres7(telefono)===true) {
+                adderror("det_usu_telefono");
+                negativeattributes("error-det-usu-telefono", 'Ingrese un telefono mayor a 6 caracteres');
+                $("#det_usu_telefono").focus();
+                $("#det_usu_telefono").keyup(KeyTelefono);
+                return false;
+            }
+            else if (maximoNumeroCaracteres9(telefono)===true) {
+                adderror("det_usu_telefono");
+                negativeattributes("error-det-usu-telefono", 'Ingrese un telefono menor a 10 caracteres');
+                $("#det_usu_telefono").focus();
+                $("#det_usu_telefono").keyup(KeyTelefono);
+                return false;
+            }
+            else if (esNum(telefono)===false) {
+                adderror("det_usu_telefono");
+                negativeattributes("error-det-usu-telefono", 'Ingrese un telefono númerico');
+                $("#det_usu_telefono").focus();
+                $("#det_usu_telefono").keyup(KeyTelefono);
+                return false;
+            }
+            else if (esnegativo(telefono)===true) {
+                adderror("det_usu_telefono");
+                negativeattributes("error-det-usu-telefono", 'Ingrese un telefono válido');
                 $("#det_usu_telefono").focus();
                 $("#det_usu_telefono").keyup(KeyTelefono);
                 return false;
@@ -165,6 +282,26 @@
             negativeattributes("error-det-usu-telefono", 'Ingrese direccion válida');
 
         }
+        else if ($valor.val() === "") {
+            adderror("det_usu_telefono");
+            negativeattributes("error-det-usu-telefono", 'Ingrese telefono');
+        }
+        else if (minimoNumeroCaracteres7($valor.val())===true) {
+            adderror("det_usu_telefono");
+            negativeattributes("error-det-usu-telefono", 'Ingrese un telefono mayor a 6 caracteres');
+        }
+        else if (maximoNumeroCaracteres9($valor.val())===true) {
+            adderror("det_usu_telefono");
+            negativeattributes("error-det-usu-telefono", 'Ingrese un telefono menor a 10 caracteres');
+        }
+        else if (esNum($valor.val()) === false) {
+            adderror("det_usu_telefono");
+            negativeattributes("error-det-usu-telefono", 'Ingrese un telefono númerico');
+        }
+        else if (esnegativo($valor.val()) === true) {
+            adderror("det_usu_telefono");
+            negativeattributes("error-det-usu-telefono", 'Ingrese un telefono válido');
+        }
         else {
             addgood("det_usu_telefono");
             attributes("error-det-usu-telefono");
@@ -177,6 +314,20 @@
             if (regular.test(correo) === false) {
                 adderror("det_usu_correo");
                 negativeattributes("error-det-usu-correo", 'Ingrese Email válido');
+                $("#det_usu_correo").focus();
+                $("#det_usu_correo").keyup(KeyCorreo);
+                return false;
+            }
+            else if (correo==="") {
+                adderror("det_usu_correo");
+                negativeattributes("error-det-usu-correo", 'Ingrese Email');
+                $("#det_usu_correo").focus();
+                $("#det_usu_correo").keyup(KeyCorreo);
+                return false;
+            }
+            else if(maximoNumeroCaracteres50(correo)===true){
+                adderror("det_usu_correo");
+                negativeattributes("error-det-usu-correo", 'Ingrese un Email menor a 50 caracteres');
                 $("#det_usu_correo").focus();
                 $("#det_usu_correo").keyup(KeyCorreo);
                 return false;
@@ -196,6 +347,14 @@
             adderror("det_usu_correo");
             negativeattributes("error-det-usu-correo", 'Ingrese Email válido');
         }
+        else if ($valor.val() === "") {
+            adderror("det_usu_correo");
+            negativeattributes("error-det-usu-correo", 'Ingrese Email');
+        }
+        else if (maximoNumeroCaracteres50($valor.val())===true) {
+            adderror("det_usu_correo");
+            negativeattributes("error-det-usu-correo", 'Ingrese un Email menor a 50 caracteres');
+        }
         else {
             addgood("det_usu_correo");
             attributes("error-det-usu-correo");
@@ -204,7 +363,7 @@
     }
      //------------------------------------------------------------------------
         function validar_det_usu_codigoColegio(colegio) {
-            var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
+            var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@0-9-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
             if (colegio.match(RegularExpression)) {
                 adderror("det_usu_codigoColegio");
                 negativeattributes("error-det-usu-colegio", 'Ingrese código válido');
@@ -212,6 +371,20 @@
                 $("#det_usu_codigoColegio").keyup(KeyColegio);
                 return false;
             }
+            else if (colegio==="") {
+                adderror("det_usu_codigoColegio");
+                negativeattributes("error-det-usu-colegio", 'Ingrese código');
+                $("#det_usu_codigoColegio").focus();
+                $("#det_usu_codigoColegio").keyup(KeyColegio);
+                return false;
+            }
+            else if (maximoNumeroCaracteres50(colegio)===true) {
+                adderror("det_usu_codigoColegio");
+                negativeattributes("error-det-usu-colegio", 'Ingrese un código de colegiatura menor a 50 caracteres');
+                $("#det_usu_codigoColegio").focus();
+                $("#det_usu_codigoColegio").keyup(KeyColegio);
+                return false;
+            } 
             else {
                 addgood("det_usu_codigoColegio");
                 attributes("error-det-usu-colegio");
@@ -220,12 +393,20 @@
         }
 
     var KeyColegio = function () {
-        var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
+        var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@0-9-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
         var $valor = $("#det_usu_codigoColegio");
-        if ($valor.val() === "") {
+        if ($valor.val().match(RegularExpression)) {
             adderror("det_usu_codigoColegio");
             negativeattributes("error-det-usu-colegio", 'Ingrese código válido');
         }
+        else if ($valor.val() === "") {
+            adderror("det_usu_codigoColegio");
+            negativeattributes("error-det-usu-colegio", 'Ingrese código');
+        }
+        else if (maximoNumeroCaracteres50($valor.val())===true) {
+            adderror("det_usu_codigoColegio");
+            negativeattributes("error-det-usu-colegio", 'Ingrese un código de colegiatura menor a 50 caracteres');
+        } 
         else {
             addgood("det_usu_codigoColegio");
             attributes("error-det-usu-colegio");
@@ -233,10 +414,24 @@
     }
      //------------------------------------------------------------------------
         function validar_det_usu_especialidad(especialidad) {
-            var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
+            var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@0-9-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
             if (especialidad === "") {
                 adderror("det_usu_especialidad");
-                negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad válido');
+                negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad');
+                $("#det_usu_especialidad").focus();
+                $("#det_usu_especialidad").keyup(KeyEspecialidad);
+                return false;
+            }
+            else if (maximoNumeroCaracteres50(especialidad)==true) {
+                adderror("det_usu_especialidad");
+                negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad menor a 50 caracteres');
+                $("#det_usu_especialidad").focus();
+                $("#det_usu_especialidad").keyup(KeyEspecialidad);
+                return false;
+            }
+          else if (especialidad.match(RegularExpression)) {
+                adderror("det_usu_especialidad");
+                negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad valida');
                 $("#det_usu_especialidad").focus();
                 $("#det_usu_especialidad").keyup(KeyEspecialidad);
                 return false;
@@ -250,11 +445,19 @@
         }
 
     var KeyEspecialidad = function () {
-        var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
+        var RegularExpression = /(^\s.*)|(.*\s{2,}.*)|.*\s$|(.*[+-\.\*@0-9-_\|/?¿?´`º!ª\\¨{\][}ç\^<>¬%&()·].*)/;
         var $valor = $("#det_usu_especialidad");
         if ($valor.val() === "") {
             adderror("det_usu_especialidad");
-            negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad válido');
+            negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad');
+        }
+        else if (maximoNumeroCaracteres50($valor.val()) == true) {
+            adderror("det_usu_especialidad");
+            negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad menor a 50 caracteres');
+        }
+       else if ($valor.val().match(RegularExpression)) {
+            adderror("det_usu_especialidad");
+            negativeattributes("error-det-usu-especialidad", 'Ingrese Especialidad valida');
         }
         else {
             addgood("det_usu_especialidad");
